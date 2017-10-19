4dacc3e7aca940c47b8d34cdb809e0d2d8c42435
Leonardo Braga <leonardokcardozo@gmail.com>
2017-10-19 10:21:53 -0300

    Fixing bugs about manhattan calculation on 8 Puzzle Board

--- Part-I/src/eight_puzzle/Board.java
+++ Part-I/src/eight_puzzle/Board.java
@@ -28,18 +28,29 @@ public class Board {
 		
 		for (int i = 0; i < this.blocks.length; i++) {
 			for (int j = 0; j < this.blocks.length; j++) {
-				if(this.blocks[i][j] != 0 && this.blocks[i][j] != ((i * n) + j + 1)){
-					hamming++;
-				}
-				
-				if(this.blocks[i][j] != 0){
-					int linha = (int)Math.ceil((double)this.blocks[i][j] / n);
-					int coluna = (this.blocks[i][j] % n == 0 ? n : (this.blocks[i][j] % n)) ;
-					
-					manhattan += Math.abs(linha - i - 1) + Math.abs(coluna - j - 1);
-				}
+				hamming += calculateHamming(i, j);
+				manhattan += calculateManhattan(i, j);
 			}
 		}
+	}
+	
+	private int calculateHamming(int i, int j){
+		if(this.blocks[i][j] != 0 && this.blocks[i][j] != ((i * n) + j + 1)){
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	private int calculateManhattan(int i, int j){
+		if(this.blocks[i][j] != 0){
+			int linha = (int)Math.ceil((double)this.blocks[i][j] / n);
+			int coluna = (this.blocks[i][j] % n == 0 ? n : (this.blocks[i][j] % n)) ;
+			
+			return Math.abs(linha - i - 1) + Math.abs(coluna - j - 1);
+		}
+		
+		return 0;
 	}
 
 	public int dimension() {
@@ -71,7 +82,7 @@ public class Board {
 			int j2 = StdRandom.uniform(n);
 			
 			if((i1 != i2 || j1 != j2) && blocksTwin.blocks[i1][j1] != 0 && blocksTwin.blocks[i2][j2] != 0){
-				exchange(blocksTwin, i1, j1, i2, j2);
+				blocksTwin.exchange(i1, j1, i2, j2);
 				found = true;
 			}
 		}
@@ -93,7 +104,7 @@ public class Board {
 							
 							if((position1 < position2 && blocks[i1][j1] > blocks[i2][j2]) || (position1 > position2 && blocks[i1][j1] < blocks[i2][j2])){
 								Board blocksTwin = new Board(blocks);
-								exchange(blocksTwin, i1, j1, i2, j2);
+								blocksTwin.exchange(i1, j1, i2, j2);
 								return blocksTwin;
 							}
 						}
@@ -106,10 +117,16 @@ public class Board {
 		return twinRandom();
 	}
 	
-	private void exchange(Board board, int i1, int j1, int i2, int j2){
-		int aux = board.blocks[i1][j1];
-		board.blocks[i1][j1] = board.blocks[i2][j2];
-		board.blocks[i2][j2] = aux;	
+	private void exchange(int i1, int j1, int i2, int j2){
+		this.hamming -= (calculateHamming(i1, j1) + calculateHamming(i2, j2));
+		this.manhattan -= (calculateManhattan(i1, j1) + calculateManhattan(i2, j2));
+		
+		int aux = this.blocks[i1][j1];
+		this.blocks[i1][j1] = this.blocks[i2][j2];
+		this.blocks[i2][j2] = aux;	
+		
+		this.hamming += (calculateHamming(i1, j1) + calculateHamming(i2, j2));
+		this.manhattan += (calculateManhattan(i1, j1) + calculateManhattan(i2, j2));
 	}
 
 	public boolean equals(Object y) {
@@ -176,25 +193,25 @@ public class Board {
 					if(blocks[i][j] == 0){
 						if(i > 0){
 							Board newboard = new Board(blocks);
-							exchange(newboard, i, j, i - 1, j);
+							newboard.exchange(i, j, i - 1, j);
 							boards.add(newboard);
 						}
 						
 						if(i < n - 1){
 							Board newboard = new Board(blocks);
-							exchange(newboard, i, j, i + 1, j);
+							newboard.exchange(i, j, i + 1, j);
 							boards.add(newboard);
 						}
 						
 						if(j > 0){
 							Board newboard = new Board(blocks);
-							exchange(newboard, i, j, i, j - 1);
+							newboard.exchange(i, j, i, j - 1);
 							boards.add(newboard);
 						}
 						
 						if(j < n - 1){
 							Board newboard = new Board(blocks);
-							exchange(newboard, i, j, i, j + 1);
+							newboard.exchange(i, j, i, j + 1);
 							boards.add(newboard);
 						}
 						
